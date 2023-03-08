230fe477c71775f87a2857d5e8e2c62692fb9d7c
thundersp <thundersp20504@gmail.com>
2023-03-08 22:42:01 +0530

    Now

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 
 df
-
+People not added files till now"
+no money shall be given;)
 
 df
 ## this is a new change
--- newwww.c
+++ newwww.c
@@ -1,19 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d;
-    b=123;
-    for(a=97;a<b;a++){
-        printf("%c",a);
-        if(a=='g'){
-            break;
-        }
-    printf("\n");}
-    for(a=97;a<b;a++){
-        printf("h%c",a);
-        if(a=='e'){
-            break;
-        }
-    printf("\n");}
+    int a;
     return 0;
 }
